5eb2458d37ec4f53585ef07873ad5caee774ada8
dorotarget <dorothee@zielinski.io>
2021-03-14 08:03:24 +0100

    wird gespawnt mit normaler velocity

--- core/src/com/doro/jumpandrun/Sprites/Extraherz.java
+++ core/src/com/doro/jumpandrun/Sprites/Extraherz.java
@@ -1,19 +1,55 @@
 package com.doro.jumpandrun.Sprites;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.utils.Array;
 import com.doro.jumpandrun.JumpAndRun;
 import com.doro.jumpandrun.Screens.PlayScreen;
 
 public class Extraherz extends PowerUp{
+    /**----------------------*/
+    private float statusZeit;
+    private Animation<TextureRegion> blinkAnimation;
+    private Array<TextureRegion> frames;
+    /**----------------------*/
     public Extraherz(PlayScreen screen, float x, float y) {
 
         super(screen, x, y);
-        setRegion(screen.getMuenzenHerzAtlas().findRegion("Herz"), 0,0,32,32);
+        //setRegion(screen.getMuenzenHerzAtlas().findRegion("Herz"), 0,0,32,32);
+        /**----------------------*/
+
+        frames = new Array<TextureRegion>();
+        for(int i = 0; i < 2; i++)
+
+            frames.add(new TextureRegion(screen.getMuenzenHerzAtlas().findRegion("Herz"), i * 64, 1, 32, 32));
+
+        blinkAnimation = new Animation(0.4f, frames);
+        statusZeit = 0;
+        setBounds(getX(), getY(), 16 / JumpAndRun.PPM, 16 / JumpAndRun.PPM);
+        /**----------------------*/
         tempo = new Vector2(0,0);
+
+
+    }
+    /**----------------------*/
+
+    public TextureRegion getFrame(float dt){
+        TextureRegion region;
+
+
+        region = blinkAnimation.getKeyFrame(statusZeit, true);
+
+        //setRegion(screen.getMuenzenHerzAtlas().findRegion("Herz"), 0, 0, 64, 64);
+        //tempo = new Vector2(0.2f, 40);
+
+        return region;
+        /**----------------------*/
+
     }
 
 
@@ -48,7 +84,12 @@ public class Extraherz extends PowerUp{
         setPosition(body.getPosition().x-getHeight()/2,body.getPosition().x-getHeight()/2);
         //setPosition(1,1);
         body.setLinearVelocity(tempo);
-    }@Override
+
+        setPosition(body.getPosition().x - ( getWidth() / 2), body.getPosition().y - getHeight() / 2);
+        setRegion(blinkAnimation.getKeyFrame(statusZeit, true));
+    }
+
+    @Override
     public void draw(Batch batch) {
         super.draw(batch);
     }
